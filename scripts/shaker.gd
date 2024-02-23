120f2bed1c562edf44a91d477461c3c37d561d5c
Qualpo <toxixbeluga@gmail.com>
2024-02-22 21:26:58 -0800

    Update shaker.gd

--- scripts/shaker.gd
+++ scripts/shaker.gd
@@ -3,7 +3,7 @@ class_name Shaker
 
 
 
-@export var Shaker : Node
+@export var shaker : Node
 var Magnitude = 0.0
 var ShakeTime = 0.0
 
@@ -20,16 +20,16 @@ func _physics_process(delta):
 	if Magnitude > 0.01:
 		ShakeTime += delta
 		Magnitude = lerp(Magnitude,0.0,0.1)
-		if Shaker is Node2D or Shaker is Control:
-			Shaker.position.x = cos(ShakeTime * 100) * Magnitude
-			Shaker.position.y = sin(ShakeTime* 75) * Magnitude
-		elif Shaker is Node3D:
-			Shaker.position.x = cos(ShakeTime * 100) * Magnitude
-			Shaker.position.y = sin(ShakeTime* 75) * Magnitude
-			Shaker.position.z = cos(ShakeTime* 75) * Magnitude
+		if shaker is Node2D or shaker is Control:
+			shaker.position.x = cos(ShakeTime * 100) * Magnitude
+			shaker.position.y = sin(ShakeTime* 75) * Magnitude
+		elif shaker is Node3D:
+			shaker.position.x = cos(ShakeTime * 100) * Magnitude
+			shaker.position.y = sin(ShakeTime* 75) * Magnitude
+			shaker.position.z = cos(ShakeTime* 75) * Magnitude
 	else:
-		if Shaker is Node2D or Shaker is Control:
-			Shaker.position = Vector2(0,0)
-		elif Shaker is Node3D:
-			Shaker.position = Vector3()
+		if shaker is Node2D or shaker is Control:
+			shaker.position = Vector2(0,0)
+		elif shaker is Node3D:
+			shaker.position = Vector3()
 		emit_signal("shake_finished")
